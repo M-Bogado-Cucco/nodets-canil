5e064a39cf2cd15e801076467edc10ddd5d890cf
Marcelo Bogado <celombc@gmail.com>
2022-10-20 20:16:50 -0300

    final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,24 @@
 import { Request, Response } from 'express'
 
+import { Pet } from '../models/Pet'
+import { createMenuObject } from '../helpers/createMenuObject'
+
 export const search = (req: Request, res: Response) => {
+
+    let query: string = req.query.q as string
+
+    // lógica para ao teclar ENTER na busca, volta a página inicial...
+    if(!query) {
+        res.redirect('/')
+        return
+    }
+
+    let list = Pet.getFromName(query)
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    })
 
 }
